a1167ad431bdc4d0d0da7fa2fd2f05b1d95c952d
Azofel <136746291+Azofel@users.noreply.github.com>
2023-07-08 23:45:37 +0300

    CSharp_DZ_4
    CSharp_DZ_4

---
+++ Задача №27/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+int copy_number = number;
+do 
+{
+    sum = sum + number%10;
+    number = number / 10;
+}
+while(number > 0);
+Console.Write($"Сумма цифр числа {copy_number} равна: {sum} ");
---
+++ Не Задача 25/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число A: ");
+int number_a = Convert.ToInt32(Console.ReadLine()); // Получили А
+
+Console.Write("Введите число B: ");
+int number_b = Convert.ToInt32(Console.ReadLine()); // Получили B
+
+
+int result = 1;
+
+for (int a = 0; a <= b; a++)
+{
+    result = result * a;
+}
+
+Console.WriteLine($"{number_a} возведя в степень {number_b} равно {result}");
